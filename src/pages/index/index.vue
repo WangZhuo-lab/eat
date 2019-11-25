0b266373d4d0e217e6a1fadb622720859dff69c9
wangzhuo <754957423@qq.com>
2019-11-25 18:16:05 +0800

    update 2

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,31 +1,29 @@
 <template>
   <div>
-  <i-grid>
-    <i-grid-item>
+  <i-grid i-class="no-border">
+    <i-grid-item v-for="grid in grids" :key="grid" i-class="no-border">
         <i-grid-icon>
-            <image src="/static/images/1.png" />
+            <image :src="grid.image" />
         </i-grid-icon>
-        <i-grid-label>外卖</i-grid-label>
-    </i-grid-item>
-    <i-grid-item>
-        <i-grid-icon>
-            <image src="/static/images/2.png" />
-        </i-grid-icon>
-        <i-grid-label>饮品</i-grid-label>
-    </i-grid-item>
-    <i-grid-item>
-        <i-grid-icon>
-            <image src="/static/images/3.png" />
-        </i-grid-icon>
-        <i-grid-label>主食</i-grid-label>
-    </i-grid-item>
-    <i-grid-item>
-        <i-grid-icon>
-            <image src="/static/images/4.png" />
-        </i-grid-icon>
-        <i-grid-label>小吃</i-grid-label>
+        <i-grid-label>{{grid.title}}</i-grid-label>
     </i-grid-item>
   </i-grid>
+   <i-panel :title="title_name">
+    <view style="padding: 15px;">
+     <i-card i-class="split" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+         <view slot="content">内容不错</view>
+         <view slot="footer">尾部内容</view>
+     </i-card>
+     <i-card i-class="split" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+         <view slot="content">内容不错</view>
+         <view slot="footer">尾部内容</view>
+     </i-card>
+     <i-card i-class="split" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+         <view slot="content">内容不错</view>
+         <view slot="footer">尾部内容</view>
+     </i-card>
+    </view>
+  </i-panel>
   </div>
 </template>
 
@@ -34,6 +32,13 @@
 export default {
   data () {
     return {
+      title_name:"热门",
+      grids:[
+        {title:"外卖",image:"/static/images/1.png"},
+        {title:"饮品",image:"/static/images/2.png"},
+        {title:"主食",image:"/static/images/3.png"},
+        {title:"小吃",image:"/static/images/4.png"}
+      ]
     }
   },
 
@@ -46,5 +51,10 @@ export default {
 </script>
 
 <style scoped>
-
+div >>>.no-border{
+  border-width:0pt;
+}
+div >>>.split{
+  margin-bottom:10pt;
+}
 </style>
