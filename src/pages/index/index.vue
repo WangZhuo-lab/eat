b7a19c2dfa82d562709ea2579d81baefc59d6f8c
wangzhuo <754957423@qq.com>
2019-11-25 14:44:31 +0800

    update wz

---
+++ src/pages/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+  <i-grid>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/1.png" />
+        </i-grid-icon>
+        <i-grid-label>外卖</i-grid-label>
+    </i-grid-item>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/2.png" />
+        </i-grid-icon>
+        <i-grid-label>饮品</i-grid-label>
+    </i-grid-item>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/3.png" />
+        </i-grid-icon>
+        <i-grid-label>主食</i-grid-label>
+    </i-grid-item>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/4.png" />
+        </i-grid-icon>
+        <i-grid-label>小吃</i-grid-label>
+    </i-grid-item>
+  </i-grid>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
